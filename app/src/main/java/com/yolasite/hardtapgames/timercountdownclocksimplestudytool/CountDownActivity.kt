23f76d1e6d9cd04e5a52485d7374f2f2b56310eb
RonnieDipple <Ronniedipple@gmail.com>
2017-10-15 18:53:13 -0500

    Added Buttons And TextViews

--- app/src/main/java/com/yolasite/hardtapgames/timercountdownclocksimplestudytool/CountDownActivity.kt
+++ app/src/main/java/com/yolasite/hardtapgames/timercountdownclocksimplestudytool/CountDownActivity.kt
@@ -4,13 +4,16 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
+import android.widget.ProgressBar
 
 class CountDownActivity : AppCompatActivity() {
+    lateinit private var pBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_count_down)
+        pBar = findViewById(R.id.progressBar)
     }
 }
