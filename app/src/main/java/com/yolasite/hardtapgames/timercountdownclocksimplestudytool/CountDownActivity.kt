1647eff1c8e30e0059251fe7d5ab8250b4cfa274
RonnieDipple <Ronniedipple@gmail.com>
2017-10-15 15:01:22 -0500

    fullScreen_NewBackground

--- app/src/main/java/com/yolasite/hardtapgames/timercountdownclocksimplestudytool/CountDownActivity.kt
+++ app/src/main/java/com/yolasite/hardtapgames/timercountdownclocksimplestudytool/CountDownActivity.kt
@@ -2,11 +2,15 @@ package com.yolasite.hardtapgames.timercountdownclocksimplestudytool
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class CountDownActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_count_down)
     }
 }
--- app/src/main/java/com/yolasite/hardtapgames/timercountdownclocksimplestudytool/MainActivity.kt
+++ app/src/main/java/com/yolasite/hardtapgames/timercountdownclocksimplestudytool/MainActivity.kt
@@ -3,15 +3,21 @@ package com.yolasite.hardtapgames.timercountdownclocksimplestudytool
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
 
         pressToStartBtn.setOnClickListener {
+
             val leagueIntent = Intent(this, CountDownActivity::class.java)
             startActivity(leagueIntent)
         }
