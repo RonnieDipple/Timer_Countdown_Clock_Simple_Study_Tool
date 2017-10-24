8a0d1b6709730f84063a467dc2f06fde9d3c0014
RonnieDipple <Ronniedipple@gmail.com>
2017-10-23 21:35:12 -0500

    This is the big Eureka I managed to show minutes and seconds, changed Input to minutes from seconds

--- app/src/main/java/com/yolasite/hardtapgames/timercountdownclocksimplestudytool/CountDownActivity.kt
+++ app/src/main/java/com/yolasite/hardtapgames/timercountdownclocksimplestudytool/CountDownActivity.kt
@@ -18,8 +18,11 @@ class CountDownActivity : AppCompatActivity() {
     lateinit private var imageViewSwitch: ImageView
     lateinit private var imageViewReset: ImageView
     lateinit private var countDownTimer: CountDownTimer
+    lateinit private var txtViewCountMinutes: TextView
     internal var isRunning = false
     internal var timeInMillis: Long = 0
+    internal var resetZero: Long = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,13 +36,14 @@ class CountDownActivity : AppCompatActivity() {
         txtViewCount = findViewById<TextView>(R.id.txtViewCount)
         imageViewSwitch = findViewById<ImageView>(R.id.imageViewSwitch)
         imageViewReset = findViewById<ImageView>(R.id.imageViewReset)
+        txtViewCountMinutes = findViewById<EditText>(R.id.txtViewCountMinutes)
 
 
 
         imageViewSwitch.setOnClickListener {
             if (!isRunning) {
                 if (editTCount.text.toString().isEmpty()) {
-                    Toast.makeText(this@CountDownActivity, "Enter number of seconds", Toast.LENGTH_LONG).show()
+                    Toast.makeText(this@CountDownActivity, "Enter number of minutes", Toast.LENGTH_LONG).show()
                 } else {
                     start()
                 }
@@ -54,7 +58,8 @@ class CountDownActivity : AppCompatActivity() {
             stop()
             isRunning = false
             imageViewSwitch.setImageResource(R.drawable.ic_start)
-            txtViewCount.text = "" + timeInMillis / 1000
+            txtViewCount.text = "" + resetZero
+            txtViewCountMinutes.text =  "" + resetZero
             pBar.progress = timeInMillis.toInt() / 1000
             pBar.max = timeInMillis.toInt() / 1000
         }
@@ -62,7 +67,7 @@ class CountDownActivity : AppCompatActivity() {
 
     private fun start() {
         val textInput = editTCount.text.toString()
-        val timeInput = textInput.toLong() * 1000
+        val timeInput = textInput.toLong() * 60 * 1000
         timeInMillis = timeInput
 
         pBar.max = timeInMillis.toInt() / 1000
@@ -72,7 +77,12 @@ class CountDownActivity : AppCompatActivity() {
 
         countDownTimer = object : CountDownTimer(timeInMillis, 100) {
             override fun onTick(millisUntilFinished: Long) {
-                txtViewCount.text = Math.round(millisUntilFinished * 0.001f).toString()
+
+                val minutes = millisUntilFinished / 1000 / 60
+                val seconds = millisUntilFinished / 1000 % 60
+
+                txtViewCountMinutes.text = minutes.toString()
+                txtViewCount.text = seconds.toString()
                 pBar.progress = Math.round(millisUntilFinished * 0.001f)
             }
 
